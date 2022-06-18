2d87a20098fd293da9774049f997b7aa577ad5b6
smyrz1-lab <smyrz1@126.com>
2022-06-18 00:41:49 -0700

    Merge remote-tracking branch 'origin/lab1-startercode'

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,13 +5,17 @@
 
 #include <cstdint>
 #include <string>
+#include <map>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
+    size_t _expected_index;
+    std::map<size_t , std::string> buffer;
+    size_t _unassembled_bytes;
+    size_t _eof;
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
 
